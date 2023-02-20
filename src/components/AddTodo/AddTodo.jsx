3e1148403fcc2a9f9bbf7d5114e34dde4772884c
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-02-20 17:54:07 +0900

    [Style] 입력폼 스타일링

--- src/components/AddTodo/AddTodo.jsx
+++ src/components/AddTodo/AddTodo.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import styles from "./AddTodo.module.css";
 
 export default function AddTodo({ onAdd }) {
   const [text, setText] = useState("");
@@ -18,14 +19,15 @@ export default function AddTodo({ onAdd }) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className={styles.form}>
       <input
         type="text"
         placeholder="Add Todo"
         value={text}
         onChange={handleChange}
+        className={styles.input}
       />
-      <button>Add</button>
+      <button className={styles.button}>Add</button>
     </form>
   );
 }
