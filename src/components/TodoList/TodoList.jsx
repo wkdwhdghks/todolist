97f7669b0b6de4ca97391d2e83fd03efd473445c
jangjonghwan <98254580+wkdwhdghks@users.noreply.github.com>
2023-02-20 15:48:02 +0900

    [Feat] 필터 적용

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
 import Todo from "../Todo/Todo";
 
-export default function TodoList() {
+export default function TodoList({ filter }) {
   const [todos, setTodos] = useState([
     { id: "1", text: "장보기", status: "active" },
     { id: "2", text: "공부하기", status: "active" },
@@ -20,10 +20,12 @@ export default function TodoList() {
     setTodos(todos.filter((t) => t.id !== deleted.id));
   };
 
+  const filtered = getFilteredItems(todos, filter);
+
   return (
     <section>
       <ul>
-        {todos.map((item) => (
+        {filtered.map((item) => (
           <Todo
             key={item.id}
             todo={item}
@@ -35,4 +37,12 @@ export default function TodoList() {
       <AddTodo onAdd={handleAdd} />
     </section>
   );
+}
+
+function getFilteredItems(todos, filter) {
+  if (filter === "all") {
+    return todos;
+  }
+
+  return todos.filter((todo) => todo.status === filter);
 }
